9871a2b5bfdb2a03650c7deaac389dfc5370c533
Shaheen Karodia <skarodia@shoprite.co.za>
2025-01-14 11:27:30 +0200

    added persistent storage

--- store/userStore.ts
+++ store/userStore.ts
@@ -1,17 +1,27 @@
 import { create } from "zustand";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 type UserStore = {
   hasFinishedOnboarding: boolean;
   toggleHasOnboarded: () => void;
 };
 
-export const useUserStore = create<UserStore>((set) => ({
-  hasFinishedOnboarding: false,
-  toggleHasOnboarded: () =>
-    set((state) => {
-      return {
-        ...state,
-        hasFinishedOnboarding: !state.hasFinishedOnboarding,
-      };
+export const useUserStore = create(
+  persist<UserStore>(
+    (set) => ({
+      hasFinishedOnboarding: false,
+      toggleHasOnboarded: () =>
+        set((state) => {
+          return {
+            ...state,
+            hasFinishedOnboarding: !state.hasFinishedOnboarding,
+          };
+        }),
     }),
-}));
+    {
+      name: "plantly-user-store",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
