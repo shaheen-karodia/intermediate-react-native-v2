f07f91e7950c4d4bc3bd91c6f571168045da4558
Shaheen Karodia <skarodia@shoprite.co.za>
2025-01-14 09:03:40 +0200

    set up expo router

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
-  ignorePatterns: ['/dist/*'],
+  extends: ["expo", "prettier"],
+  plugins: ["prettier", "react-native"],
+  ignorePatterns: ["/dist/*"],
+  rules: {
+    "prettier/prettier": ["error"],
+    "react-native/no-unused-styles": "error",
+  },
 };
